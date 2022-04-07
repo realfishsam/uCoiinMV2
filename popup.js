acbf5d115535a8a1e5e25603f327d19cae46b41b
realfishsam <92118699+realfishsam@users.noreply.github.com>
2022-04-07 19:37:19 +0200

    Update popup.js

--- popup.js
+++ popup.js
@@ -1,11 +1,45 @@
 window.onload = function() {
-    foo = document.getElementById("throttle");
-    // foo.value = 40; // This works
+    throttle = document.getElementById("throttle");
 
     chrome.storage.local.get(["id", "thr"], function(result) {
         thr = result.thr;
+        throttle.value = thr;
 
-        foo.value = thr;
+        id = result.id;
+        async function getUSD(usrid) {
+            usdconversion = await fetch("http://ucoiin.com/convert");
+            usdconversion = await usdconversion.json();
+            usdconversion = usdconversion.price
+
+            ting = await fetch(`http://ucoiin.com/user?id=${usrid}`)
+            ting = await ting.json();
+            console.log(ting.message.pending);
+    
+            // tenmillion = 10000000
+    
+            // now convert hashes to mintme
+            mint = ting.message.pending / 10000000
+    
+            usd = Math.round(mint * usdconversion * 100) / 100;
+            console.log(usd);
+
+            money = document.getElementById("money");
+            money.innerHTML = `$${usd}`; 
+            money.setAttribute("class", "white-text");
+
+        }        
+        usd = getUSD(id);
+
+
+        if(typeof id === undefined) {
+            console.log("User is not signed in.")
+            document.getElementById("main").innerHTML = "Please sign in via https://ucoiin.com/signin"
+
+        }
+
+        else {
+            return;
+        };
 
         });
 
